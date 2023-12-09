32cb59a6703ad3d2133f944106f6ef19dc26fe61
yunus <yyas404@gmail.com>
2023-12-10 00:21:03 +0530

    jwt authentication

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,31 +1,80 @@
 import asyncHandler from "express-async-handler";
+import User from "../models/userModel.js";
+import generateToken from "../utils/generateToken.js";
 
 // Auth user/set token
 // POST /api/users/auth
 // public
 const authUser = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: "Auth " });
+  const { email, password } = req.body;
+  const user = await User.findOne({ email });
+
+  if (user && (await user.matchPassword(password))) {
+    generateToken(res, user._id);
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+    });
+  } else {
+    res.status(401);
+    throw new Error("Invalid email or password");
+  }
 });
 
 // Register a new user
 // POST /api/users
 // public
 const registerUser = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: "register user " });
+  const { name, email, password } = req.body;
+  const userExists = await User.findOne({ email });
+  if (userExists) {
+    res.status(400);
+    throw new Error("User already exists");
+  }
+
+  const user = await User.create({
+    name,
+    email,
+    password,
+  });
+
+  if (user) {
+    generateToken(res, user._id);
+
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+    });
+  } else {
+    res.status(400);
+    throw new Error("Invalid user data");
+  }
 });
 
 // Logout user
 // POST /api/users/logout
 // public
 const logoutUser = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: "logout user " });
+  res.cookie("jwt", "", {
+    httpOnly: true,
+    expires: new Date(0),
+  });
+  res.status(200).json({ message: "user logged out  " });
 });
 
 // get user profile
 // GET /api/users/profile
 // private
 const getUserProfile = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: "user profile" });
+  const user = {
+    _id: req.user._id,
+    name: req.user.name,
+    email: req.user.email,
+  };
+
+  res.status(200).json(user);
 });
 
 // Update user profile
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,4 +1,5 @@
 import express from "express";
+const router = express.Router();
 import {
   authUser,
   registerUser,
@@ -6,11 +7,14 @@ import {
   getUserProfile,
   updateUserProfile,
 } from "../controllers/userController.js";
-const router = express.Router();
+import { protect } from "../middleware/authMiddleware.js";
 
 router.post("/auth", authUser);
 router.post("/", registerUser);
 router.post("/logout", logoutUser);
-router.route("/profile").get(getUserProfile).put(updateUserProfile);
+router
+  .route("/profile")
+  .get(protect, getUserProfile)
+  .put(protect, updateUserProfile);
 
 export default router;
