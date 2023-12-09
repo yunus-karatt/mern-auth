d355829019437dcfde1e33d15dcf931bd6ac7661
yunus <yyas404@gmail.com>
2023-12-09 17:55:30 +0530

    basic routes

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,10 +1,44 @@
-import asyncHandler from 'express-async-handler'
+import asyncHandler from "express-async-handler";
 
 // Auth user/set token
 // POST /api/users/auth
 // public
-const authUser =asyncHandler( async (req, res) => {
+const authUser = asyncHandler(async (req, res) => {
   res.status(200).json({ message: "Auth " });
 });
 
-export { authUser };
+// Register a new user
+// POST /api/users
+// public
+const registerUser = asyncHandler(async (req, res) => {
+  res.status(200).json({ message: "register user " });
+});
+
+// Logout user
+// POST /api/users/logout
+// public
+const logoutUser = asyncHandler(async (req, res) => {
+  res.status(200).json({ message: "logout user " });
+});
+
+// get user profile
+// GET /api/users/profile
+// private
+const getUserProfile = asyncHandler(async (req, res) => {
+  res.status(200).json({ message: "user profile" });
+});
+
+// Update user profile
+// PuT /api/users/profile
+// private
+const updateUserProfile = asyncHandler(async (req, res) => {
+  res.status(200).json({ message: "update user profile" });
+});
+
+export {
+  authUser,
+  registerUser,
+  logoutUser,
+  getUserProfile,
+  updateUserProfile,
+};
