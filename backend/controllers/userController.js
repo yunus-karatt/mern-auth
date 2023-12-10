0dfd6a03d253b3817fb14797fa43616df4945c0e
yunus <yyas404@gmail.com>
2023-12-10 19:13:55 +0530

    frontend signup,login

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -81,7 +81,25 @@ const getUserProfile = asyncHandler(async (req, res) => {
 // PuT /api/users/profile
 // private
 const updateUserProfile = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: "update user profile" });
+    const user=await User.findById(req.user._id);
+    if(user){
+      user.name=req.body.name||user.name
+      user.email=req.body.email||user.email
+
+      if(req.body.password){
+        user.password=req.body.password
+      }
+
+      const updatedUser=await user.save()
+      res.status(200).json({
+        _id:updatedUser._id,
+        name:updatedUser.name,
+        email:updatedUser.email,
+      })
+    }else{
+      res.status(404);
+      throw new Error('user not found')
+    }
 });
 
 export {
